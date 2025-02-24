9b3d13183b3bccafe929302af4bba3c933ce9bd4
evandvance <evandvance@gmail.com>
2025-02-24 14:39:46 -0600

    Begin User Model Shape

---
+++ backend/send_me/modules/users/models.py
@@ -0,0 +1,19 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+
+from send_me.database.models import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
+    username: Mapped[str]
+    email: Mapped[str]
+
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), nullable=False, default=datetime.now
+    )
