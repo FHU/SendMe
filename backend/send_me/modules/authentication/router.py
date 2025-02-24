a50985586fe239109a6cd98285335e75e1439e37
evandvance <evandvance@gmail.com>
2025-02-24 15:50:39 -0600

    Really fix randbelow

--- backend/send_me/modules/authentication/router.py
+++ backend/send_me/modules/authentication/router.py
@@ -30,7 +30,7 @@ def request_pin(input: schemas.LoginRequest, db: Session = Depends(get_db)):
 
     # Create a login
     item = models.Login(
-        pin=str(secrets.randbelow(0,999999)),
+        pin=str(secrets.randbelow(1_000_000)),
         token=str(secrets.token_urlsafe(16)),
         email=input.email,
     )
