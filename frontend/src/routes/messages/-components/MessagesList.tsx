e000df85ae33b5b4d0c6e27b7f29a50ea01ca37e
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2025-02-26 16:31:27 -0600

    More spacing fixes

--- frontend/src/routes/messages/-components/MessagesList.tsx
+++ frontend/src/routes/messages/-components/MessagesList.tsx
@@ -6,8 +6,8 @@ import React from "react";
 const messages = [
 	{imagePath: '../../../public/images/christian-buehner-DItYlc26zVI-unsplash.jpg', userName: 'John Smith', lastReadMessage: "Hey! I heard about Servant's Day and would love to contribute...", lastReadTime: '3:53 P.M.', isRead: false},
 	{imagePath: '../../../public/images/microsoft-365-7mBictB_urk-unsplash.jpg', userName: 'Clara Donovan', lastReadMessage: "Hello! My name is Clara and I was wondering if there might...", lastReadTime: '1:23 P.M.', isRead: false},
-	{imagePath: '../../public/images/karl-fredrickson-JRsZWmRd_Ws-unsplash.jpg', userName: 'Estes CoC', lastReadMessage: "Hi! I’m interested in helping with the food drive and I thought that...", lastReadTime: '7:23 A.M.', isRead: true},
-	{imagePath: '../../public/images/cosmic-timetraveler-_R1cc2IHk70-unsplash.jpg', userName: 'Henderson CoC', lastReadMessage: "That sounds doable! Do I need to make an appointment with an elder or can I...", lastReadTime: '2:42 P.M.', isRead: true}
+	{imagePath: '../../public/images/karl-fredrickson-JRsZWmRd_Ws-unsplash.jpg', userName: 'Estes Church of Christ', lastReadMessage: "Hi! I’m interested in helping with the food drive and I thought that...", lastReadTime: '7:23 A.M.', isRead: true},
+	{imagePath: '../../public/images/cosmic-timetraveler-_R1cc2IHk70-unsplash.jpg', userName: 'Henderson Church of Christ', lastReadMessage: "That sounds doable! Do I need to make an appointment with an elder or can I...", lastReadTime: '2:42 P.M.', isRead: true}
 ]
 
 const MessageCard = styled.div`
@@ -55,17 +55,17 @@ const Message: React.FC<MessageProps> = ({
 					style={{gridRowStart: '1', gridRowEnd: "3", placeSelf: "center", paddingTop: '10px'}}
 					></SlAvatar>
 				{isRead ? <>
-				<h2 style={{gridRowStart: "1", gridRowEnd: '3', gridColumnStart: "2", paddingTop: '5px', color:"gray"}}>{userName}</h2>
-				<p style={{gridRowStart: "2", gridRowEnd: '3', gridColumnStart:"2", gridColumnEnd: "5", color:"gray"}}>{lastReadMessage}</p>
-				<p style={{gridRowStart: "1", gridColumnStart:"4", color:"gray"}}>{lastReadTime}</p>
+				<h2 style={{gridRowStart: "1", gridRowEnd: '3', gridColumnStart: "2", gridColumnEnd: '4', color:"gray"}}>{userName}</h2>
+				<p style={{gridRowStart: "2", gridRowEnd: '3', gridColumnStart:"2", gridColumnEnd: "5", paddingTop: '40px', color:"gray"}}>{lastReadMessage}</p>
+				<p style={{gridRowStart: "1", gridRowEnd: '3', gridColumnStart:"4", color:"gray", paddingTop: '6px'}}>{lastReadTime}</p>
 				</> : 
 				<>
-				<h2 style={{gridRowStart: "1", gridRowEnd: '3', gridColumnStart: "2", paddingTop: '5px'}}>{userName}</h2>
-				<p style={{gridRowStart: "2", gridRowEnd: '3', gridColumnStart:"2", gridColumnEnd: "5"}}>{lastReadMessage}</p>
-				<p style={{gridRowStart: "1", gridColumnStart:"4"}}>{lastReadTime}</p>		
+				<h2 style={{gridRowStart: "1", gridRowEnd: '3', gridColumnStart: "2", gridColumnEnd: '4'}}>{userName}</h2>
+				<p style={{gridRowStart: "2", gridRowEnd: '3', gridColumnStart:"2", gridColumnEnd: "5", paddingTop: '40px'}}>{lastReadMessage}</p>
+				<p style={{gridRowStart: "1", gridRowEnd: '3', gridColumnStart:"4", paddingTop: '6px'}}>{lastReadTime}</p>		
 				</>}
 
-				{isRead ? <ReadButton style={{gridColumn: '5', gridRow:'2', backgroundColor: 'white'}}/> : <ReadButton style={{gridColumn: '5', gridRow:'2'}}/>}
+				{isRead ? <ReadButton style={{gridColumn: '5', gridRow:'2', backgroundColor: 'white'}}/> : <ReadButton style={{gridColumn: '5', gridRow:'2', alignSelf: 'center', marginTop: '15px'}}/>}
 			</MessageCard>
 
 	</div>
