4e4dc0062fd414c50580e0ccfb057180a9db379d
PearceWalker <100614334+PearceWalker@users.noreply.github.com>
2025-03-03 13:48:51 -0600

    Routed auth to Frontend. (Now accesible on /auth)

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -10,85 +10,104 @@
 
 // Import Routes
 
-import { Route as rootRoute } from "./routes/__root";
-import { Route as IndexImport } from "./routes/index";
-import { Route as OpportunitiesIndexImport } from "./routes/opportunities/index";
+import { Route as rootRoute } from './routes/__root'
+import { Route as IndexImport } from './routes/index'
+import { Route as OpportunitiesIndexImport } from './routes/opportunities/index'
+import { Route as AuthIndexImport } from './routes/auth/index'
 
 // Create/Update Routes
 
 const IndexRoute = IndexImport.update({
-	id: "/",
-	path: "/",
-	getParentRoute: () => rootRoute,
-} as any);
+  id: '/',
+  path: '/',
+  getParentRoute: () => rootRoute,
+} as any)
 
 const OpportunitiesIndexRoute = OpportunitiesIndexImport.update({
-	id: "/opportunities/",
-	path: "/opportunities/",
-	getParentRoute: () => rootRoute,
-} as any);
+  id: '/opportunities/',
+  path: '/opportunities/',
+  getParentRoute: () => rootRoute,
+} as any)
+
+const AuthIndexRoute = AuthIndexImport.update({
+  id: '/auth/',
+  path: '/auth/',
+  getParentRoute: () => rootRoute,
+} as any)
 
 // Populate the FileRoutesByPath interface
 
-declare module "@tanstack/react-router" {
-	interface FileRoutesByPath {
-		"/": {
-			id: "/";
-			path: "/";
-			fullPath: "/";
-			preLoaderRoute: typeof IndexImport;
-			parentRoute: typeof rootRoute;
-		};
-		"/opportunities/": {
-			id: "/opportunities/";
-			path: "/opportunities";
-			fullPath: "/opportunities";
-			preLoaderRoute: typeof OpportunitiesIndexImport;
-			parentRoute: typeof rootRoute;
-		};
-	}
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/auth/': {
+      id: '/auth/'
+      path: '/auth'
+      fullPath: '/auth'
+      preLoaderRoute: typeof AuthIndexImport
+      parentRoute: typeof rootRoute
+    }
+    '/opportunities/': {
+      id: '/opportunities/'
+      path: '/opportunities'
+      fullPath: '/opportunities'
+      preLoaderRoute: typeof OpportunitiesIndexImport
+      parentRoute: typeof rootRoute
+    }
+  }
 }
 
 // Create and export the route tree
 
 export interface FileRoutesByFullPath {
-	"/": typeof IndexRoute;
-	"/opportunities": typeof OpportunitiesIndexRoute;
+  '/': typeof IndexRoute
+  '/auth': typeof AuthIndexRoute
+  '/opportunities': typeof OpportunitiesIndexRoute
 }
 
 export interface FileRoutesByTo {
-	"/": typeof IndexRoute;
-	"/opportunities": typeof OpportunitiesIndexRoute;
+  '/': typeof IndexRoute
+  '/auth': typeof AuthIndexRoute
+  '/opportunities': typeof OpportunitiesIndexRoute
 }
 
 export interface FileRoutesById {
-	__root__: typeof rootRoute;
-	"/": typeof IndexRoute;
-	"/opportunities/": typeof OpportunitiesIndexRoute;
+  __root__: typeof rootRoute
+  '/': typeof IndexRoute
+  '/auth/': typeof AuthIndexRoute
+  '/opportunities/': typeof OpportunitiesIndexRoute
 }
 
 export interface FileRouteTypes {
-	fileRoutesByFullPath: FileRoutesByFullPath;
-	fullPaths: "/" | "/opportunities";
-	fileRoutesByTo: FileRoutesByTo;
-	to: "/" | "/opportunities";
-	id: "__root__" | "/" | "/opportunities/";
-	fileRoutesById: FileRoutesById;
+  fileRoutesByFullPath: FileRoutesByFullPath
+  fullPaths: '/' | '/auth' | '/opportunities'
+  fileRoutesByTo: FileRoutesByTo
+  to: '/' | '/auth' | '/opportunities'
+  id: '__root__' | '/' | '/auth/' | '/opportunities/'
+  fileRoutesById: FileRoutesById
 }
 
 export interface RootRouteChildren {
-	IndexRoute: typeof IndexRoute;
-	OpportunitiesIndexRoute: typeof OpportunitiesIndexRoute;
+  IndexRoute: typeof IndexRoute
+  AuthIndexRoute: typeof AuthIndexRoute
+  OpportunitiesIndexRoute: typeof OpportunitiesIndexRoute
 }
 
 const rootRouteChildren: RootRouteChildren = {
-	IndexRoute: IndexRoute,
-	OpportunitiesIndexRoute: OpportunitiesIndexRoute,
-};
+  IndexRoute: IndexRoute,
+  AuthIndexRoute: AuthIndexRoute,
+  OpportunitiesIndexRoute: OpportunitiesIndexRoute,
+}
 
 export const routeTree = rootRoute
-	._addFileChildren(rootRouteChildren)
-	._addFileTypes<FileRouteTypes>();
+  ._addFileChildren(rootRouteChildren)
+  ._addFileTypes<FileRouteTypes>()
 
 /* ROUTE_MANIFEST_START
 {
@@ -97,11 +116,15 @@ export const routeTree = rootRoute
       "filePath": "__root.tsx",
       "children": [
         "/",
+        "/auth/",
         "/opportunities/"
       ]
     },
     "/": {
       "filePath": "index.tsx"
+    },
+    "/auth/": {
+      "filePath": "auth/index.tsx"
     },
     "/opportunities/": {
       "filePath": "opportunities/index.tsx"
---
+++ frontend/src/routes/auth/index.tsx
@@ -0,0 +1,18 @@
+import api from "@sendme/api";
+import { SlSpinner } from "@shoelace-style/shoelace/dist/react";
+import { createFileRoute } from "@tanstack/react-router";
+import styled from "styled-components";
+import RequestPinForm from "./-components/Auth";
+
+export const Route = createFileRoute("/auth/")({
+	component: RouteComponent,
+});
+
+
+function RouteComponent() {
+	return (
+
+		<RequestPinForm />
+		
+	);
+}
