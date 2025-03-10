e33d4a099bb97ecff5f691e098a157a8f766e5c4
brigittet <89213142+brigittet@users.noreply.github.com>
2025-03-10 16:10:32 -0500

    Delete unecessary imports

--- frontend/src/routes/home/index.tsx
+++ frontend/src/routes/home/index.tsx
@@ -1,9 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import styled from "styled-components";
 import Background from "./-components/background";
-import Card from "./-components/card";
-import Card1 from "./-components/card1";
-import Card2 from "./-components/card2";
 
 export const Route = createFileRoute("/home/")({
 	component: RouteComponent,
