f6ece0c5428e113e892365b3ad026c6a4b83b22f
Andrew Maach <andy@andrewmaach.com>
2025-03-17 20:04:05 -0500

    lock qraft cli version

--- frontend/src/routes/opportunities/index.tsx
+++ frontend/src/routes/opportunities/index.tsx
@@ -6,19 +6,20 @@ import { CreateOpportunity } from "./-components/CreateOpportunity";
 import { OpportunitiesList } from "./-components/OpportunitiesList";
 
 export const Route = createFileRoute("/opportunities/")({
-	component: RouteComponent,
+  component: RouteComponent,
 });
 
 const AreaHeading = styled.h2``;
 
 function RouteComponent() {
-	const { orgs, refetchOrg } = api.organizations.list_organizations.useQuery();
-	const { data, refetch } = api.opportunities.list_opportunities.useQuery();
-	return (
-		<>
-			<CreateOpportunity onCreated={refetch} />
-			<AreaHeading>Opportunities</AreaHeading>
-			{!data ? <SlSpinner /> : <OpportunitiesList data={data} />}
-		</>
-	);
+  const { data: orgs, refetch: refetchOrg } =
+    api.organizations.listOrganizations.useQuery();
+  const { data, refetch } = api.opportunities.listOpportunities.useQuery();
+  return (
+    <>
+      <CreateOpportunity onCreated={refetch} />
+      <AreaHeading>Opportunities</AreaHeading>
+      {!data ? <SlSpinner /> : <OpportunitiesList data={data} />}
+    </>
+  );
 }
