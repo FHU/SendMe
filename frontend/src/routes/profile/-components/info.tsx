bda40debe45d7b8eb3d7c5923f4aff4b65e0c512
Priscilla Harris <89226780+PHarris2002@users.noreply.github.com>
2025-04-02 15:55:23 -0500

    Biome Fixes

--- frontend/src/routes/profile/-components/info.tsx
+++ frontend/src/routes/profile/-components/info.tsx
@@ -3,6 +3,6 @@ import styled from "styled-components";
 const InfoContainer = styled.div`
 `;
 
-const Info = () => <InfoContainer></InfoContainer>;
+const Info = () => <InfoContainer />;
 
 export default Info;
