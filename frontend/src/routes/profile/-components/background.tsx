a1c6138be7a94202dc20cee7fe587a1eaf7b0889
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2025-04-02 15:10:09 -0500

    commit

--- frontend/src/routes/profile/-components/background.tsx
+++ frontend/src/routes/profile/-components/background.tsx
@@ -4,10 +4,14 @@ import Tags from "./tags";
 import Info from "./info";
 import PastProj from "./past_proj";
 
-const BackGround = styled.div`
+const BackGround = styled.div
+` background: var(--sl-color-primary-50);
+  padding: 10px;
   display: flex;
   flex-direction: column;
-`;
+  align-items: center
+  max-width: fit-content`
+;
 
 const Background = () => (
 	<BackGround>
