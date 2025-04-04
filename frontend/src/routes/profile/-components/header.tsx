659c84b1d2df33a68ba1369a01224433bee05a0d
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2025-04-04 12:05:25 -0500

    Update header.tsx

--- frontend/src/routes/profile/-components/header.tsx
+++ frontend/src/routes/profile/-components/header.tsx
@@ -1,8 +1,27 @@
+import {
+    SlAvatar
+} from "@shoelace-style/shoelace/dist/react";
 import styled from "styled-components";
 
 const HeaderContainer = styled.div`
+    position: absolute;
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 600px;
+    background-color: var(--sl-color-primary-200);
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 30px;
 `;
 
-const Header = () => <HeaderContainer></HeaderContainer>;
+const SLAvatar = styled.div`
+    width: 80px;
+    height: 80px;
+    border-radius: 50%;
+`
+
+const Header = () => <HeaderContainer>
+    <SLAvatar></SLAvatar>
+</HeaderContainer>;
 
 export default Header;
