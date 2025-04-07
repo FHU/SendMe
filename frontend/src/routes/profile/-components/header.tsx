4dcecd15fa5dfd16c866bd90c4488609bf841049
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2025-04-07 15:59:14 -0500

    commit

--- frontend/src/routes/profile/-components/header.tsx
+++ frontend/src/routes/profile/-components/header.tsx
@@ -4,36 +4,32 @@ import {
 import styled from "styled-components";
 
 const HeaderContainer = styled.div`
-    position: absolute;
+
     display: flex;
     flex-direction: column;
     width: 400px;
     height: 600px;
-    background-color: var(--sl-color-primary-200);
+    background-color: var(--sl-color-primary-500);
     border-bottom-left-radius: 30px;
     border-bottom-right-radius: 30px;
 `;
 
-const SLAvatar = styled.div`
-    width: 80px;
-    height: 80px;
-    border-radius: 50%;
-`;
-
 const ProfileUsername = styled.h1`
     text-align: center;
-    padding-top: .5rem;
-    padding-bottom: .5rem;
-    font-size: 40px;
+    padding-top: 0;
+    padding-bottom: 0;
+    font-size: 30px;
+    color: white;
 `;
 
 const ConnectionNumber = styled.p`
     text-align: center;
-    font-size: 10px;
+    font-size: 15px;
+    color: white;
 `;
 
 const Header = () => <HeaderContainer>
-    <SLAvatar image="images/christian-buehner-DItYlc26zVI-unsplash.jpg"/>
+    <SlAvatar image="images/christian-buehner-DItYlc26zVI-unsplash.jpg"/>
     <ProfileUsername>Christian Buehner</ProfileUsername>
     <ConnectionNumber>999 Connections</ConnectionNumber>
 
