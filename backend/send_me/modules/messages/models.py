5d864e0bf6073f1031c86bcbd2aab93bbe29d59a
Wesley Baker <89235616+WeslBaker@users.noreply.github.com>
2025-04-07 20:26:34 -0500

    Added essential Conversation and Message models

--- backend/send_me/modules/messages/models.py
+++ backend/send_me/modules/messages/models.py
@@ -2,7 +2,7 @@ import uuid
 from datetime import datetime
 
 from sqlalchemy import DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, ForeignKey
 
 from send_me.database.models import Base
 
@@ -17,8 +17,10 @@ class Message(Base):
     __tablename__ = "messages"
 
     id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.uuid4)
-    isUser: Mapped[bool]
-    text: Mapped[str]
+    sender: Mapped[uuid.UUID] = mapped_column(
+        ForeignKey = "users.id")
+    )
+    content: Mapped[str]
     conversation_id: Mapped[uuid.UUID]
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), nullable=False, default=datetime.now
