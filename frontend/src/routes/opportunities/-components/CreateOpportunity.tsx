50a0b949fcabd69daf78550a1bfdb645525dedac
Addison Adcock <89235734+AddisonAdcock@users.noreply.github.com>
2025-04-08 18:59:20 -0500

    Added Fields

--- frontend/src/routes/opportunities/-components/CreateOpportunity.tsx
+++ frontend/src/routes/opportunities/-components/CreateOpportunity.tsx
@@ -46,15 +46,19 @@ export function CreateOpportunity({ onCreated }: { onCreated: () => void }) {
 	return (
 		<Form onSubmit={onSubmit}>
 			<Heading>Share Opportunity</Heading>
-			<Input disabled={isPending} label="Title" name="name" />
-			<Input disabled={isPending} label="Description" name="description" />
-			<Input disabled={isPending} label="Tags" name="description" />
-			<Input disabled={isPending} label="Summary" name="description" />
-			<Input disabled={isPending} label="Location" name="description" />
-			<Input disabled={isPending} label="Event Date" name="description" />
-			<Input disabled={isPending} label="Event Time" name="description" />
-			<Input disabled={isPending} label="Position" name="description" />
-			<Input disabled={isPending} label="Position" name="description" />
+			<Input disabled={isPending} label="Title" name="name" required />
+			<Input
+				disabled={isPending}
+				label="Description"
+				name="description"
+				required
+			/>
+			<Input disabled={isPending} label="Tags" name="tags" placeholder="e.g., remote, full-time" />
+			<Input disabled={isPending} label="Summary" name="summary"/>
+			<Input disabled={isPending} label="Location" name="location" placeholder="City, State" />
+			<Input disabled={isPending} label="Event Date" name="eventDate" type="date" />
+			<Input disabled={isPending} label="Event Time" name="eventTime" type="time" />
+			<Input disabled={isPending} label="Position" name="position" placeholder="e.g., Software Engineer" />
 			<ToTheRight>
 				<SlButton type="submit" loading={isPending}>
 					Share
