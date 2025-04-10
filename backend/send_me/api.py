f240a4da8b0bc4b640d6c5c661036b03f118b48a
Evan Vance <72946268+evandvance@users.noreply.github.com>
2025-04-10 14:20:51 -0500

    Add type ignores
    Adopted a new technology with stricter typing. Sendgrid (as far as I am aware) does not provide the support for typing. Could be wrong, but not a rabit worth chasing.

--- backend/send_me/api.py
+++ backend/send_me/api.py
@@ -2,6 +2,7 @@ from fastapi import Depends, FastAPI
 
 from send_me.database.router import router as database_router
 from send_me.modules.authentication.dependencies import get_session
+from send_me.modules.authentication.models import Session
 from send_me.modules.authentication.router import router as authorization_router
 from send_me.modules.opportunities.router import router as opportunities_router
 from send_me.modules.organizations.router import router as organizations_router
@@ -27,5 +28,5 @@ async def hello_world():
 
 
 @app.get("/protected")
-async def protected_example(session=Depends(get_session)):
+async def protected_example(session: Session =Depends(get_session)):
     return {"message": "protected content"}
