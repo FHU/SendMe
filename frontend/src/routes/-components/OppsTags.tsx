2b88ba3046ade8601a7a9f8d1c85aa581a90c9af
gavboler03 <89207920+gavboler03@users.noreply.github.com>
2025-04-16 15:28:24 -0500

    Ran biome

--- frontend/src/routes/-components/OppsTags.tsx
+++ frontend/src/routes/-components/OppsTags.tsx
@@ -1,8 +1,8 @@
 import {
-  SlButton,
-  SlButtonGroup,
-  SlIcon,
-  SlTag,
+	SlButton,
+	SlButtonGroup,
+	SlIcon,
+	SlTag,
 } from "@shoelace-style/shoelace/dist/react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -15,56 +15,56 @@ import "slick-carousel/slick/slick-theme.css";
 `;*/
 
 const tags = [
-  "Preacher",
-  "Youth",
-  "Education",
-  "Leadership",
-  "Paid",
-  "Education",
-  "Leadership",
-  "Paid",
-  "Leadership",
-  "Paid",
-  "Education",
-  "Leadership",
-  "Paid",
+	"Preacher",
+	"Youth",
+	"Education",
+	"Leadership",
+	"Paid",
+	"Education",
+	"Leadership",
+	"Paid",
+	"Leadership",
+	"Paid",
+	"Education",
+	"Leadership",
+	"Paid",
 ];
 
 const OppTags = () => {
-  const settings = {
-    dots: false,
-    infinite: false,
-    speed: 250,
-    slidesToShow: 5,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: { slidesToShow: 5 },
-      },
-      {
-        breakpoint: 600,
-        settings: { slidesToShow: 5 },
-      },
-    ],
-  };
-  return (
-    <>
-      <div
-        style={{ width: "100%", padding: "20px", backgroundColor: "#121212" }}
-      >
-        <Slider {...settings}>
-          {tags.map((tag, id) => (
-            <div key={id}>
-              <SlTag variant="success" size="medium">
-                {tag}
-              </SlTag>
-            </div>
-          ))}
-        </Slider>
-      </div>
-    </>
-  );
+	const settings = {
+		dots: false,
+		infinite: false,
+		speed: 250,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: { slidesToShow: 5 },
+			},
+			{
+				breakpoint: 600,
+				settings: { slidesToShow: 5 },
+			},
+		],
+	};
+	return (
+		<>
+			<div
+				style={{ width: "100%", padding: "20px", backgroundColor: "#121212" }}
+			>
+				<Slider {...settings}>
+					{tags.map((tag, id) => (
+						<div key={tag}>
+							<SlTag variant="success" size="medium">
+								{tag}
+							</SlTag>
+						</div>
+					))}
+				</Slider>
+			</div>
+		</>
+	);
 };
 
 export default OppTags;
