c598138feac8dde309673ac8d2e2a5766c347a1a
Addison Adcock <89235734+AddisonAdcock@users.noreply.github.com>
2025-04-17 17:03:21 -0500

    TEST BIOME STUFF

--- frontend/src/routes/home/create/-components/CreateOpportunity.tsx
+++ frontend/src/routes/home/create/-components/CreateOpportunity.tsx
@@ -184,8 +184,6 @@ export function CreateOpportunity({ onCreated }: { onCreated: () => void }) {
 							disabled={isPending}
 						/>
 					</TimeOfEventContainer>
-					
-					{/* Button */}
 					<ButtonContainer>
 						<SubmitButton type="submit" disabled={isPending}>
 							{isPending ? "Sharing..." : "Share"}
