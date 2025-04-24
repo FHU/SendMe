e8265c410d5d48edab9727109e4530e47a77d869
gavboler03 <89207920+gavboler03@users.noreply.github.com>
2025-04-24 18:49:50 -0500

    Added one button

--- frontend/src/routes/-components/OppsTags.tsx
+++ frontend/src/routes/-components/OppsTags.tsx
@@ -1,10 +1,15 @@
-import { SlTag } from "@shoelace-style/shoelace/dist/react";
+import {
+  SlTag,
+  SlButtonGroup,
+  SlButton,
+  SlIcon,
+} from "@shoelace-style/shoelace/dist/react";
 import { useState } from "react";
 import styled from "styled-components";
 
 const ScrollerWrapper = styled.div`
   width: 80%;
-  padding: 20px;
+  gap: 10px;
 `;
 const TagsScroller = styled.div`
   display: flex;
@@ -30,65 +35,70 @@ const StyledTag = styled(SlTag)`
 `;
 
 const OppTags = () => {
-	const [selectedTags, setSelectedTags] = useState<string[]>([]);
-	const [unselectedTags, setUnselectedTags] = useState<string[]>([
-		"Preacher",
-		"Youth",
-		"Education",
-		"Leadership",
-		"Paid",
-		"Evangelism",
-		"Volunteer",
-		"Hospitality",
-		"Singing",
-		"Construction",
-		"Lawn",
-		"Landscaping",
-	]);
-	const handleRemoveTag = (tagToRemove: string) => {
-		setSelectedTags((tags) => tags.filter((tag) => tag !== tagToRemove));
-		setUnselectedTags((tags) => [...tags, tagToRemove]);
-	};
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const [unselectedTags, setUnselectedTags] = useState<string[]>([
+    "Preacher",
+    "Youth",
+    "Education",
+    "Leadership",
+    "Paid",
+    "Evangelism",
+    "Volunteer",
+    "Hospitality",
+    "Singing",
+    "Construction",
+    "Lawn",
+    "Landscaping",
+  ]);
+  const handleRemoveTag = (tagToRemove: string) => {
+    setSelectedTags((tags) => tags.filter((tag) => tag !== tagToRemove));
+    setUnselectedTags((tags) => [...tags, tagToRemove]);
+  };
 
-	const handleAddTag = (tagToAdd: string) => {
-		setUnselectedTags((tags) => tags.filter((tag) => tag !== tagToAdd));
-		setSelectedTags((tags) => [...tags, tagToAdd]);
-	};
-	return (
-		<>
-			<ScrollerWrapper>
-				<TagsScroller>
-					{selectedTags.map((tag) => (
-						<TagWrapper key={`selected-${tag}`}>
-							<StyledTag
-								variant="success"
-								size="medium"
-								removable
-								pill
-								onSlRemove={() => handleRemoveTag(tag)}
-							>
-								{tag}
-							</StyledTag>
-						</TagWrapper>
-					))}
-				</TagsScroller>
-				<TagsScroller>
-					{unselectedTags.map((tag) => (
-						<TagWrapper key={`unselected-${tag}`}>
-							<StyledTag
-								variant="success"
-								size="medium"
-								pill
-								onClick={() => handleAddTag(tag)}
-							>
-								{tag}
-							</StyledTag>
-						</TagWrapper>
-					))}
-				</TagsScroller>
-			</ScrollerWrapper>
-		</>
-	);
+  const handleAddTag = (tagToAdd: string) => {
+    setUnselectedTags((tags) => tags.filter((tag) => tag !== tagToAdd));
+    setSelectedTags((tags) => [...tags, tagToAdd]);
+  };
+  return (
+    <>
+      <ScrollerWrapper>
+        <SlButtonGroup>
+          <SlButton pill variant="success">
+            <SlIcon name="arrow-left" />
+          </SlButton>
+        </SlButtonGroup>
+        <TagsScroller>
+          {selectedTags.map((tag) => (
+            <TagWrapper key={`selected-${tag}`}>
+              <StyledTag
+                variant="success"
+                size="medium"
+                removable
+                pill
+                onSlRemove={() => handleRemoveTag(tag)}
+              >
+                {tag}
+              </StyledTag>
+            </TagWrapper>
+          ))}
+        </TagsScroller>
+        <TagsScroller>
+          {unselectedTags.map((tag) => (
+            <TagWrapper key={`unselected-${tag}`}>
+              <StyledTag
+                variant="success"
+                size="medium"
+                pill
+                onClick={() => handleAddTag(tag)}
+              >
+                {tag}
+              </StyledTag>
+            </TagWrapper>
+          ))}
+        </TagsScroller>
+      </ScrollerWrapper>
+    </>
+  );
 };
 
 export default OppTags;
